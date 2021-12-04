c83a1f106e5d8ea5702eea95066fde7bc1d4dc76
Sourabh <100rabhMalewade@gmail.com>
2021-12-05 01:24:23 +0530

    dashboard added

--- src/index.js
+++ src/index.js
@@ -4,18 +4,40 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Login from "./components/Login"
-import Profile from "./components/Profile"
+import Login from "./Pages/Login"
+import Profile from "./Pages/Profile"
+import Team from './Pages/Team';
+import Reports from './Pages/Reports';
+import Talk from './Pages/Talk';
+import Support from './Pages/Support';
+import Message from './Pages/Message';
+import Home from './Pages/Home';
+
+import Navbar from './components/Navbar';
+import Footer from './components/Footer'
+
+
 
 ReactDOM.render(
   <React.StrictMode>
        <BrowserRouter>
+       <Navbar />
+       <div className='page-container'>
+      <div className='content-wrap'></div>
       <Routes>
-        <Route path="/" element={<App />}></Route>
+        <Route path='/' element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/profile" element={<Profile />}></Route>
+        <Route path='/reports' element={<Reports />} />
+        <Route path='/talk' element={<Talk />} />
+        <Route path='/team' element={<Team />} />
+        <Route path='/support' element={<Support />} />
+        <Route path='/message' element={<Message />} /> 
       </Routes>
+      </div>
+      <Footer />
     </BrowserRouter>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
