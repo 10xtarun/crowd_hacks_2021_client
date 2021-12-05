e43e30221277cc4236ca08fc49c52001762a0e0b
hrithik16032000 <hrithikgavankar8582@gmail.com>
2021-12-05 13:34:40 +0530

    stream colums

--- src/Pages/Stream.js
+++ src/Pages/Stream.js
@@ -2,161 +2,221 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import config from "../config.json";
 import "./Stream.css";
-import { Col, Row, Container, Card, Button } from "react-bootstrap";
+import { Col, Row, Container, Card, ListGroup } from "react-bootstrap";
 
 const Stream = () => {
-
-    const [playlistIds, setplaylistIds] = useState(null)
-    const [tracks, setTracks] = useState(null)
-    const [vibes, setVibes] = useState(null)
-    const [recents, setRecents] = useState(null)
+    const [playlistIds, setplaylistIds] = useState(null);
+    const [tracks, setTracks] = useState(null);
+    const [vibes, setVibes] = useState(null);
+    const [recents, setRecents] = useState(null);
 
     const getplaylistIds = () => {
-        axios.get(config.api.url + "/user/profile", {
-            headers: {
-                Authorization: localStorage.getItem("jtoken")
-            }
-        })
-            .then(response => {
-                if (response.status === 200) setplaylistIds(response.data.profile.playlists)
-                else throw Error("server request not success")
+        axios
+            .get(config.api.url + "/user/profile", {
+                headers: {
+                    Authorization: localStorage.getItem("jtoken"),
+                },
             })
-            .catch(err => {
-                alert(err.toString())
+            .then((response) => {
+                if (response.status === 200)
+                    setplaylistIds(response.data.profile.playlists);
+                else throw Error("server request not success");
             })
-    }
+            .catch((err) => {
+                alert(err.toString());
+            });
+    };
 
     const getPlaylistTracks = () => {
-        axios.get(config.api.url + "/streams/tracks", {
-            headers: {
-                Authorization: localStorage.getItem("jtoken")
-            }
-        })
-            .then(response => {
+        axios
+            .get(config.api.url + "/streams/tracks", {
+                headers: {
+                    Authorization: localStorage.getItem("jtoken"),
+                },
+            })
+            .then((response) => {
                 if (response.status === 200) {
-                    if (response.data.tracks.length < 1) throw Error("you don't have any tracks")
-                    setTracks(response.data.tracks)
-                } else
-                    throw Error("server request not success")
+                    if (response.data.tracks.length < 1)
+                        throw Error("you don't have any tracks");
+                    setTracks(response.data.tracks);
+                } else throw Error("server request not success");
             })
-            .catch(err => {
-                alert(err.toString())
-            })
-    }
+            .catch((err) => {
+                alert(err.toString());
+            });
+    };
 
     const getRecent = () => {
-        axios.get(config.api.url + "/streams/recent", {
-            headers: {
-                Authorization: localStorage.getItem("jtoken")
-            }
-        })
-            .then(response => {
-                console.log(response)
+        axios
+            .get(config.api.url + "/streams/recent", {
+                headers: {
+                    Authorization: localStorage.getItem("jtoken"),
+                },
+            })
+            .then((response) => {
+                console.log(response);
                 if (response.status === 200) {
-                    setVibes(response.data.vibes)
-                    setRecents(response.data.tracks)
-                } else
-                    throw Error("server request not success")
+                    setVibes(response.data.vibes);
+                    setRecents(response.data.tracks);
+                } else throw Error("server request not success");
             })
-            .catch(err => {
-                alert(err.toString())
-            })
-    }
+            .catch((err) => {
+                alert(err.toString());
+            });
+    };
 
     useEffect(() => {
         if (playlistIds) {
-            getPlaylistTracks()
-            getRecent()
+            getPlaylistTracks();
+            getRecent();
         }
-    }, [playlistIds])
+    }, [playlistIds]);
 
     return (
         <div id="stream-page">
-            {
-                !localStorage.getItem("jtoken") ?
-                    window.location.href = "/login" : null
-            }
+            {!localStorage.getItem("jtoken")
+                ? (window.location.href = "/login")
+                : null}
+
             <Container>
                 <Row>
+                    <h1> Streams</h1>
                     <Col>
                         <div className="card">
                             <div className="box">
                                 <div className="content">
-                                    <h3>Playlist</h3>
-                                    <button onClick={getplaylistIds}>Refresh your Playlist</button>
+                                    <h3>Access your music profile</h3>
+                                    {/* <button onClick={getplaylistIds}>Refresh</button> */}
+                                    <button
+                                        className="button-36"
+                                        onClick={getplaylistIds}
+                                    >
+                                        Refresh
+                                    </button>
                                 </div>
                             </div>
                         </div>
                     </Col>
+                    <div></div>
                     <Col>
-                        <div>
 
-                            <h1>Streams</h1>
-                            <hr />
-                            <button onClick={getplaylistIds}>Refresh Your Playlist</button>
-                            <hr />
+                    <div>
+                        <Row>
+
+                            <Col>
                             {/* PLAYLIST SECTION */}
-                            {
-                                playlistIds &&
+                                    {playlistIds && (
+                                        <Card style={{ width: "18rem" }}>
+                                        <Card.Header>Your Playlists</Card.Header>
+                                        <ListGroup variant="flush">
+                                        <ListGroup.Item>
                                 <ul>
-                                    {
-                                        playlistIds.map((pl, index) => {
-                                            return <li key={index}><p>
-                                                {pl.name}
-                                            </p></li>
-                                        })
-                                    }
+                                    {playlistIds.map((pl, index) => {
+                                        return (
+                                            <li key={index}>
+                                                <p>{pl.name}</p>
+                                            </li>
+            
+                                        );
+                                    })}
                                 </ul>
-                            }
+                                </ListGroup.Item>
+                                </ListGroup>
+                            </Card>
+
+                            )}
+                        </Col>    
+                            
+                        
                             {/* PLAYLIST SECTION */}
-                            {
-                                tracks &&
+                        <Col>
+                                    {tracks && (
+                                        <Card style={{ width: "18rem" }}>
+                                        <Card.Header>Your Tracks</Card.Header>
+                                        <ListGroup variant="flush">
+                                            <ListGroup.Item>
                                 <ul>
-                                    {
-                                        tracks.map((tr, index) => {
-                                            return <li key={index}><p>
-                                                {tr.name}
-                                                {
-                                                    tr.artists.map((art, x) => {
-                                                        return <span key={x}>{art} </span>
-                                                    })
-                                                }
-                                            </p></li>
-                                        })
-                                    }
+                                    {tracks.map((tr, index) => {
+                                        return (
+                                            
+                                            <li key={index}>
+                                                <p>
+                                                    {tr.name}
+                                                    {tr.artists.map(
+                                                        (art, x) => {
+                                                            return (
+                                                                <span key={x}>
+                                                                    {art}{" "}
+                                                                </span>
+                                                            );
+                                                        }
+                                                    )}
+                                                </p>
+                                            </li>
+                                    
+                                        );
+                                    })}
                                 </ul>
-                            }
-                            <hr />
-                            {
-                                recents &&
+                                </ListGroup.Item>
+                                </ListGroup>
+                            </Card>
+                            )}
+                        </Col>
+                       </Row>
+                       
+                    <div>
+                        <Row>
+                            <Col>  
+                                    {recents && (
+                                        <Card style={{ width: "18rem" }}>
+                                        <Card.Header>Recently Played</Card.Header>
+                                        <ListGroup variant="flush">
+                                            <ListGroup.Item>
                                 <ul>
-                                    {
-                                        recents.map((tr, index) => {
-                                            return <li key={index}>
+                                    {recents.map((tr, index) => {
+                                        return (
+                                            <li key={index}>
                                                 <p>{tr}</p>
                                             </li>
-                                        })
-                                    }
+                                            
+                                        );
+                                    })}
                                 </ul>
+                                </ListGroup.Item>
+                                </ListGroup>
+                            </Card>
+                            )}
 
-                            }
-                            <hr />
-                            {
-                                vibes &&
+                            </Col>
+                            
+                            <Col>
+                                    {vibes && (
+                            <Card style={{ width: "18rem" }}>
+                            <Card.Header>Your Vibes</Card.Header>
+                            <ListGroup variant="flush">
+                                <ListGroup.Item>
                                 <div>
                                     <p>danceability : {vibes.danceability}</p>
                                     <p>acousticness : {vibes.acousticness}</p>
                                     <p>energy : {vibes.energy}</p>
                                 </div>
-
-                            }
-                            <hr />
+                                </ListGroup.Item>
+                                </ListGroup>
+                            </Card>
+                            )}
+                            </Col>
+                        </Row>  
+                    </div>      
+    
                         </div>
                     </Col>
                 </Row>
+                <br />
+                <br />
+
             </Container>
         </div>
     );
 };
 
-export default Stream
+export default Stream;
